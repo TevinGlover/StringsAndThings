1064bf65ad73595dba1b39e3bf746f5012ecebfe
Tevin Glover <tevwork.tg@gmail.com>
2023-11-01 23:45:27 -0400

    im getting better

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -8,7 +8,7 @@ public class StringsAndThings {
 
     /**
      * Given a string, count the number of words ending in 'y' or 'z' -- so the 'y' in "heavy" and the 'z' in "fez" count,
-     * but not the 'y' in "yellow" (not case sensitive). We'll say that a y or z is at the end of a word if there is not an alphabetic
+     * but not the 'y' in "yellow" (not case-sensitive). We'll say that a y or z is at the end of a word if there is not an alphabetic
      * letter immediately following it. (Note: Character.isLetter(char) tests if a char is an alphabetic letter.)
      * example : countYZ("fez day"); // Should return 2
      * countYZ("day fez"); // Should return 2
@@ -33,17 +33,17 @@ public class StringsAndThings {
         }
         return count;// return
     }
-}
-    /*
-     * Given two strings, base and remove, return a version of the base string where all instances of the remove string have
-     * been removed (not case sensitive). You may assume that the remove string is length 1 or more.
+
+
+     /** Given two strings, base and remove, return a version of the base string where all instances of the remove string have
+     * been removed (not case-sensitive). You may assume that the remove string is length 1 or more.
      * Remove only non-overlapping instances, so with "xxx" removing "xx" leaves "x".
      *
      * example : removeString("Hello there", "llo") // Should return "He there"
      *           removeString("Hello there", "e") //  Should return "Hllo thr"
      *           removeString("Hello there", "x") // Should return "Hello there"
      */
-    /*
+
     public String removeString(String base, String remove){
         
 
@@ -52,14 +52,14 @@ public class StringsAndThings {
 
     /**
      * Given a string, return true if the number of appearances of "is" anywhere in the string is equal
-     * to the number of appearances of "not" anywhere in the string (case sensitive)
+     * to the number of appearances of "not" anywhere in the string (case-sensitive)
      *
      * example : containsEqualNumberOfIsAndNot("This is not")  // Should return false
      *           containsEqualNumberOfIsAndNot("This is notnot") // Should return true
      *           containsEqualNumberOfIsAndNot("noisxxnotyynotxisi") // Should return true
      */
 
-    /*
+
     public Boolean containsEqualNumberOfIsAndNot(String input){
         return null;
     }
@@ -72,7 +72,7 @@ public class StringsAndThings {
      *           gHappy("xxggyygxx") // Should return  false
      */
 
-    /*
+
     public Boolean gIsHappy(String input){
         return null;
     }
@@ -85,9 +85,8 @@ public class StringsAndThings {
      *            countTriple("xxxabyyyycd") // Should return 3
      *            countTriple("a") // Should return 0
      */
-    /*
+
     public Integer countTriple(String input){
         return null;
     }
 }
-*/
